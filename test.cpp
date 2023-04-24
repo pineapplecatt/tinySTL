308c846f58c63df77660047174e7536a7175f0ea
pineapplecatt <mayijing828@gmail.com>
2023-04-24 16:23:28 +0800

    提交1

--- test.cpp
+++ test.cpp
@@ -1,7 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int mian(){
-    cout<<"hello"<<endl;
+int main(){
+    int n;
+    cin >> n;
+    int m[n][n]{0};
+    for (int i = 0; i < n; i++) {
+        for (int j = n-1;j>=i;j--){
+            m[i][j]=1;
+        }
+    }
+    for (int i = 0;i<n;i++){
+        for (int j = 0;j<n;j++){
+            cout << m[i][j]<<" ";
+        }
+        cout << endl;
+    }
     return 0;
-
 }
